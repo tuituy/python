cce39e29d08e0458f1dea081fb427d3f2f51159d
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-08-19 22:47:19 -0300

    Add files via upload

--- ex008.py
+++ ex008.py
@@ -1,4 +1,4 @@
-metros = int(input('Digite um Valor em metros: '))
-centimetros = metros / 100
-milimetros = centimetros /100
+metros = float(input('Digite um Valor em metros: '))
+centimetros = metros * 100
+milimetros = centimetros * 100
 print ('O Valor em centimetros é {}, e o valor em milímetros é {}.'.format(centimetros,milimetros))
