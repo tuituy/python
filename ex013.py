2919009778f5557b7e309e18b7bdc84ba72995c0
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-08-20 22:11:17 -0300

    Add files via upload

--- ex013.py
+++ ex013.py
@@ -1,4 +1,3 @@
-sal = float(input('Qual é o seu salário?'))
+sal = float(input('Qual é o seu salário? R$'))
 aum = sal*0.15
-
-print('Seu novo salário com 15% de aumento é {}reais, parabéns!'.format(sal+aum))
+print('Seu novo salário com 15% de aumento é R${:.2f}reais, parabéns!'.format(sal+aum))
