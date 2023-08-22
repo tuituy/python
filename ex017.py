786bebc6667f011a87726fd5b95d7ba8afd49e83
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-08-21 22:25:16 -0300

    Add files via upload

---
+++ ex017.py
@@ -0,0 +1,11 @@
+from math import hypot
+co = float(input('Qual é o comprimento do cateto oposto?'))
+ca = float(input('Qual é o comprimento do cateto adjacente?'))
+
+hi = hypot(co,ca)
+
+
+
+
+
+print ('O comprimento da hipotenusa é {:.2f}'.format(hi))
