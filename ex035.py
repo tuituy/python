d26afca494499f9be5925b9c59ce197b4f51f860
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-08-25 22:48:06 -0300

    Add files via upload

---
+++ ex035.py
@@ -0,0 +1,9 @@
+r1 = float(input('Digite a primeira reta '))
+r2 = float(input('Digite a segunda reta '))
+r3 = float(input('Digite a terceira reta '))
+
+
+if  r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Boa, temos um triângulo!!')
+else:
+    print('Isso ai n dá um triângulo não!')
