a7fea3690d6d2376b32d4294b217b6cecef6b429
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-08-27 21:05:42 -0300

    Add files via upload

---
+++ aula11.py
@@ -0,0 +1,7 @@
+ansi
+\033[0;30;41m #fundo vermelho, texto branco
+\033[4;33;44m #fundo azul, texto amarelo, sublinhado
+\033[1;35;43m #fundo amarelo, texto roxo,
+\033[30;42m #fundo verde, texto cinza
+\033[m
+\033[7;30m
---
+++ aula11a.py
@@ -0,0 +1,4 @@
+print('\033[1;31;40mOlá Mundo\033[m')
+print('\033[32;40m{}\033[m'.format(4))
+nome = 'Arthur'
+print('Olá, Muito prazer em te conhecer, {}{}{}!!!'.format('\033[7;40m' ,nome,'\033[m'))
