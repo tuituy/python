66687d97b71c0dc27365879ea945d24ef39669d8
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-08-29 21:51:33 -0300

    Add files via upload

--- ex040.py
+++ ex040.py
@@ -3,8 +3,8 @@ nota2 = float(input('Digite a sua segunda nota: '))
 media = (nota1 + nota2) / 2
 
 if media < 5.0:
-    print('Sua média é {}, você está reprovado!!'.format(media))
-elif media >= 5.0 and media <= 6.9:
-    print('Sua média é {}, você está de recuperação!!'.format(media))
-elif media >= 7.0 and media <= 10:
-    print('Sua média é {}, você foi aprovado!!'.format(media))
+    print('Sua média é {:.1f}, você está reprovado!!'.format(media))
+elif 6.9 >= media >= 5.0 :
+    print('Sua média é {:.1f}, você está de recuperação!!'.format(media))
+elif 10 >= media >= 7.0 :
+    print('Sua média é {:.1f}, você foi aprovado!!'.format(media))
