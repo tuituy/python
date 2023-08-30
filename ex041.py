3b798c86dd3c76d174b3793baa2c341893e32c31
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-08-29 21:59:47 -0300

    Add files via upload

--- ex041.py
+++ ex041.py
@@ -1,13 +1,15 @@
+from datetime import date
 ano = int(input('Qual o ano de nascimento do atleta? '))
-idade = 2023 - ano
-
+atual = date.today().year
+idade = atual - ano
+print('O atleta tem {} anos.'.format(idade))
 if idade <= 9 :
     print('Categoria MIRIM.')
-elif idade <= 14 and idade > 9:
+elif idade <= 14:
     print('Categoria INFANTIL.')
-elif idade <=19 and idade > 14:
+elif idade <=19:
     print('Categoria JUNIOR.')
-elif idade == 20:
+elif idade <= 25:
     print('Categoria SÊNIOR')
-else:
+elif idade >25:
     print('Categoria MASTER.')
