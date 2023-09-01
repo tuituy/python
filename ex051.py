4860b4e40cffe556ba6782716f191ad621ce9cff
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-08-31 22:40:23 -0300

    Add files via upload

---
+++ ex051.py
@@ -0,0 +1,9 @@
+ptermo = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razao termo: '))
+fim = 10
+cont = 0
+for c in range ((ptermo),1000, (razao)):
+    if cont < 10:
+        print(c)
+        cont = cont +1
+
