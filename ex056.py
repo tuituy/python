dd5ccdd8ef84e07545e253244f3d8c5bac680ab5
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-01 23:04:46 -0300

    Add files via upload

---
+++ ex056.py
@@ -0,0 +1,31 @@
+homemvelho = ''
+idadegeral = 0 #f
+idadegeralmedia= (idadegeral / 4) #f
+idadehomemvelho = 0
+
+idadem = 0 #f
+
+cont = 1
+for c in range (1,5):
+    n = str(input('Digite o {}º nome: '.format(c)))
+    i = int(input('Digite a {}º idade:'.format(c)))
+    sexo = int(input(''' Escolha o Sexo:
+    [ 1 ] Masculino
+    [ 2 ] Feminino
+    '''))
+
+    idadegeral = idadegeral + i
+    print(idadegeral)
+    if sexo == 2 and i < 20 :
+        idadem = idadem + 1
+    elif sexo == 1  and i > idadehomemvelho:
+        homemvelho = (n)
+        idadehomemvelho = (i)
+    elif sexo < 1 or sexo > 2 :
+        print('Código inválido')
+
+
+print('A média de idade do grupo é {:.1f}, o nome do homem mais velho é o {} e existe {} mulheres com menos de 20 anos.'.format(idadegeral/4,homemvelho,idadem))
+
+
+
