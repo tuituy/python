889ea3d8cbe8a7e5e54e2a56bf8614aadcf32b0c
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-02 21:51:24 -0300

    Add files via upload

--- ex049.py
+++ ex049.py
@@ -1,10 +1,8 @@
 n = int(input('Digite um número inteiro '))
-print('Você escolheu a tabuada do {}'.format(n))
+print('Você escolheu a tabuada do {}.'.format(n))
 for c in range (1,11):
-    r = n * c
-    print('-='*7)
-    print('{''} x {} = {}'.format(c,n,r))
 
+    print('-='*6)
+    print('{} x {} = {}'.format(c,n,n*c))
 
-    r = r+1
-print('-='*7)
+print('-='*6)
