3f481f635e963d7e4cffad612cb83c9cfc31251d
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-02 21:44:03 -0300

    Add files via upload

--- ex048.py
+++ ex048.py
@@ -1,6 +1,8 @@
 s = 0
+cont = 0
 for c in range (1,500,2):
-    
+
     if c % 3 == 0:
-        s = s + c
-print(s)
+        s += c
+        cont += 1
+print('A soma dos valores divisíveis por 3 é de {} e a quantidade de valores somados é {}.'.format(s,cont))
