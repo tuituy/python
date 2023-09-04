a7e6e66f7f34fa356d1dc4522115d79091e9ffcb
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-03 21:30:49 -0300

    Add files via upload

--- ex054.py
+++ ex054.py
@@ -2,13 +2,13 @@ from datetime import date
 atual = date.today().year
 contadormenor = 0
 contadormaior = 0
-for c in range (0,7):
+for c in range (1,8):
 
-    ano = int(input('Qual ano você nasceu? '))
+    ano = int(input('Qual ano a {}ª pessoa nasceu? '.format(c)))
     if atual - ano <= 21:
         contadormenor = contadormenor + 1
 
     else:
         contadormaior = contadormaior +1
 
-print('{} são maiores de idade e {} são menores de idade.'.format(contadormaior,contadormenor))
+print('{} são maiores de idade\nE {} são menores de idade.'.format(contadormaior,contadormenor))
