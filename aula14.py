ddc1541dce03392a862b46cfa18fcf6aca80249c
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-04 20:44:32 -0300

    Add files via upload

---
+++ aula14.py
@@ -0,0 +1,17 @@
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N]')). upper()
+print('Fim')
+
+n = 1
+par = 0
+impar = 0
+while n != 0:
+    if n "= 0:"
+        n int(input('Digite um valor: '))
+        if n % 2 ==0:
+            par += 1
+        else:
+            impar += 1
+print('Acabou')
