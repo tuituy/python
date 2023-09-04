ed9b870952f2a160311ecc0c3a3feb324f8c64bd
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-03 21:51:42 -0300

    Add files via upload

--- ex055.py
+++ ex055.py
@@ -1,18 +1,23 @@
 cont = 1
 maior = 0
 menor = 0
-for c in range (0,5):
+for c in range (1,6):
     peso = float(input('Digite o {}º peso: '.format(cont)))
     cont = cont +1
-
-    if peso > maior:
+    if c == 1:
         maior = peso
-
-    elif peso < maior:
         menor = peso
+    else:
+
+        if peso > maior:
+            maior = peso
+
+        elif peso < menor:
+            menor = peso
 
 
 
 
-print('o maior peso foi o Kg {:.2f} e o menor foi Kg {:.2f}'.format(maior,menor))
+
+print('o maior peso foi o Kg {:.2f}\ne o menor foi Kg {:.2f}'.format(maior,menor))
 
