4bb07225536eba42114b604747ee3046aa52bf60
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-04 21:23:03 -0300

    Add files via upload

---
+++ ex058.py
@@ -0,0 +1,26 @@
+from random import randint
+from time import sleep
+computador = randint(0,10) # pc escolhe um número
+cont = 1
+tentativas = 0
+print('*-'*21)
+print('Vou pensar em um número entre 0 e 10. Tente adivinhar...')
+print('*-'*21)
+
+while cont != 0:
+    jogador = int(input('Em que número eu pensei? '))
+    print('PROCESSANDO...')
+    sleep(1)
+    if jogador == computador:
+        cont = 0
+        print('\033[30;42mPARABÉNS! Você consegiu me vencer!, Realmente pensei no {}\033[m'.format(computador))
+        print('Você precisou de {} tentativas.'.format(tentativas))
+
+    else:
+        print('\033[0;30;41mErrou,Tente novamente!\033[m')
+        tentativas = tentativas + 1
+        
+
+
+
+
