2a95e92703dd498949584e0e72dc1d8c452a1fff
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-05 22:49:03 -0300

    Add files via upload

--- ex061.py
+++ ex061.py
@@ -1,15 +1,10 @@
 ptermo = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razao termo: '))
 
-a1 = ptermo
-a2 = razao
 
 fim = 0
 while fim != 10:
-    a2 = a1 + a2
-    if fim == 0:
-        print ('{}'.format(a1), end=' ')
-    else:
-        print('{}'.format(a2),end=' ')
+    print ('{}'.format(ptermo), end=' ')
+    ptermo += razao
     fim += 1
 print('Fim')
