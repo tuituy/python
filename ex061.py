256cee341ce01f7ecf3ae5b3080f737e960c6d8b
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-06 22:20:22 -0300

    Add files via upload

--- ex061.py
+++ ex061.py
@@ -2,19 +2,17 @@ ptermo = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razao termo: '))
 
 
-fim = 10
-while fim != 0:
+fim = 0
+while fim != 10:
     print ('{}'.format(ptermo), end=' ')
     ptermo += razao
-    fim -= 1
-    if fim == 0:
-
-        proxrazao = int(input('Se deseja continuar digite quantos mais, ou digite 0 para sair: '))
-        if fim > 0:
-            print('{}'.format(ptermo), end=' ')
-            fim = 0
-            ptermo += razao
-            fim -= 1
-       
+    fim += 1
+    if fim == 1:
+        proxtermo = int(input('Se deseja continuar digite quantos mais, ou digite 0 para sair: '))
+        fim = 0
+        ptermo += razao
+        fim += 1
 
 
+
+print('Fim')
---
+++ ex062.py
@@ -0,0 +1,15 @@
+ptermo = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razao termo: '))
+
+
+fim = 10
+while fim > 0:
+    print ('{}'.format(ptermo), end=' ')
+    ptermo += razao
+    fim -= 1
+    if fim == 0:
+        fim = int(input('Se deseja continuar digite quantos mais, ou digite 0 para sair: '))
+        if fim == 0:
+            print('Fim')
+            fim = -1
+
