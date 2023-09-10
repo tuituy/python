0c0edec1df694b7c06afc679272a5d385e4d740e
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-09 21:38:29 -0300

    Add files via upload

--- ex058.py
+++ ex058.py
@@ -2,7 +2,7 @@ from random import randint
 from time import sleep
 computador = randint(0,10) # pc escolhe um número
 cont = 1
-tentativas = 0
+tentativas = 1
 print('*-'*21)
 print('Vou pensar em um número entre 0 e 10. Tente adivinhar...')
 print('*-'*21)
@@ -17,6 +17,10 @@ while cont != 0:
         print('Você precisou de {} tentativas.'.format(tentativas))
 
     else:
+        if jogador < computador:
+            print('é mais...')
+        elif jogador > computador:
+            print('é menos...')
         print('\033[0;30;41mErrou,Tente novamente!\033[m')
         tentativas = tentativas + 1
         
