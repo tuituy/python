c18eaa2dc515b803ea2096fe93522eef7fde5db6
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-11 22:06:37 -0300

    Add files via upload

---
+++ ex067.py
@@ -0,0 +1,16 @@
+parar = False
+num = 1
+print('-'*21)
+while True:
+
+    num = int(input('Quer ver a tabuada de qual valor? '))
+    print('-'*21)
+    if num < 0:
+        break
+    for c in range (1,11):
+        print(f'{num} x {c} = {c*num}')
+    print('-'*21)
+
+
+
+print('PROGRAMA TABUADA ENCERRADO. Volte sempre!')
