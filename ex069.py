0a3326ef9367ce32850910f113673f226d9c2c00
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-11 22:47:13 -0300

    Add files via upload

---
+++ ex069.py
@@ -0,0 +1,32 @@
+maioridade = 0
+homens = 0
+mulhermenor = 0
+
+
+
+
+
+while True:
+    print('_' * 25)
+    print('   CADASTRE UMA PESSOA      ')
+    print('_' * 25)
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo: [M/F]')).upper().strip()
+    resp = 'S'
+
+
+    resp = str(input('Quer continuar? [S/N]')).upper().strip()
+
+    if idade > 18:
+        maioridade += 1
+    if sexo in 'mM':
+        homens +=1
+    if idade < 18 and sexo in 'Ff':
+        mulhermenor +=1
+    if resp in 'nN':
+        break
+
+print('======= FIM DO PROGRAMA =======')
+print(f'Total de pessoas com mais de 18 anos: {maioridade}.')
+print(f'Ao todo temos {homens} homens cadastrados.')
+print(f'E temos {mulhermenor} mulheres com menos de 20 anos.')
