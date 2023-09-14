ddb2d9a48f13db028ac6d2579aa1b1b6e26f66c0
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-13 21:51:50 -0300

    Add files via upload

--- ex066.py
+++ ex066.py
@@ -1,12 +1,11 @@
-num2 = 0
+
 soma = 0
 cont = 0
 while True:
-    num1 = int(input('Digite um valor (999 para parar): '))
-    if num1 == 999:
+    num = int(input('Digite um valor (999 para parar): '))
+    if num == 999:
         break
-    soma = num1 + num2
-    num2 = soma
+    soma += num
     cont += 1
 print(f'A soma dos {cont} valores foi {soma}!')
 
