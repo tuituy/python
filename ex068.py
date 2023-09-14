5d5989a01246152a56020b8cf3b0100125ace7e2
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-13 22:47:21 -0300

    Add files via upload

--- ex068.py
+++ ex068.py
@@ -2,29 +2,37 @@ from random import randint
 print('=-'*15)
 print('VAMOS JOGAR PAR OU ÍMPAR')
 print('=-'*15)
-cont = 0
-total = 0
 
+cont = 0
 while True:
+
+    total = 0
+    tipo = '2'
+
     jogador = int(input('Diga um valor: '))
-    escolha = str(input('Par ou Ímpar? [P/I]')).upper()
+
     computador = int(randint(1,10))
     total = jogador + computador
 
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar? [P/I]')).upper().strip()[0]
 
-    if escolha == 'P' and total % total == 0:
-        print('_' * 20)
-        print(f'Você jogou {jogador} e o computador {computador}. Total de {total}, DEU PAR')
-        print('_' * 20)
-        print('Você VENCEU!')
-        print('Vamos jogar novamente ...')
-        cont += 1
-    else:
-        print('_' * 20)
-        print(f'Você jogou {jogador} e o computador {computador}. Total de {total}, DEU IMPAR')
-        print('_' * 20)
-        print('Você PERDEU!')
-        print('_' * 20)
-        break
+    print(f'Você jogou {jogador} e o computador jogou {computador}. Total de {total}', 'DEU PAR' if total % 2== 0 else 'DEU IMPAR')
+    if tipo == 'P':
+       if total % 2 == 0:
+           print('Você VENCEU!!')
+           cont += 1
+       else:
+           print('Você PERDEU!')
+           break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!')
+            cont += 1
+        else:
+            print('Você PERDEU!!')
+            break
+
+
 print(f'GAME OVER! Você venceu {cont} vezes. ')
 
