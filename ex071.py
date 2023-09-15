afbdad912e9a041d5031458b516562b5c0bcbfed
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-14 22:22:10 -0300

    Add files via upload

--- ex071.py
+++ ex071.py
@@ -1,6 +1,6 @@
-print('='*40)
-print('                BANCO CEV')
-print('='*40)
+print('='*30)
+print('{:^30}'.format('BANCO CEV'))
+print('='*30)
 valor = int(input('Que valor você quer sacar? R$'))
 
 r50 = 0
@@ -8,24 +8,24 @@ r20 = 0
 r10 = 0
 r1 = 0
 
-while valor > 50:
+
+while valor >= 50:
     r50 += 1
     valor -= 50
 
-
-while valor > 20:
+while valor >= 20 and valor < 50:
     r20 += 1
     valor -= 20
 
-while valor > 10:
+while valor >= 10 and valor < 20:
     r10 += 1
     valor -= 10
 
-while valor >= 1:
+while valor >= 1 and valor < 10:
     r1 += 1
     valor -= 1
 
-
+print(valor)
 
 
 print(f'''Total de {r50} cédulas de R$50
