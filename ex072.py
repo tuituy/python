0a4bf2d8152a96d82d0a23c843fa6f1d9fedfdc4
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-15 22:56:06 -0300

    Add files via upload

---
+++ ex072.py
@@ -0,0 +1,4 @@
+n = ('Zero','Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quatorze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
+num = int(input('Digite um número entre 0 e 20: '))
+print(f'Você digitou o número {n[num]}')
+
