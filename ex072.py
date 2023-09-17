54f92e6b06856b5cce072e350eeca4b63e164388
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-16 21:35:19 -0300

    Add files via upload

--- ex072.py
+++ ex072.py
@@ -1,4 +1,6 @@
 n = ('Zero','Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quatorze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
-num = int(input('Digite um número entre 0 e 20: '))
+num = ' '
+while num not in (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20):
+    num = int(input('Digite um número entre 0 e 20: '))
 print(f'Você digitou o número {n[num]}')
 
