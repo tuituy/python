28468e71d91fcfe13d05d9a615f182170452de84
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-17 22:56:14 -0300

    Add files via upload

--- ex075.py
+++ ex075.py
@@ -1,45 +1,14 @@
-
-num1 = int(input('Digite um número: '))
-num2 = int(input('Digite outro número: '))
-num3 = int(input('Digite mais um número: '))
-num4 = int(input('Digite o último número: '))
-numeros = (num1,num2,num3,num4)
-
-numero9 = 0
-pares = 0
-if num1  % 2 == 0:
-    pares += 1
-elif num2 % 2 == 0:
-    pares += 1
-elif num3 % 2 == 0:
-    pares += 1
-elif num4 % 2 == 0:
-    pares += 1
-
-
-
-
-print(f'Você digitou os valores {numeros}')
-if num1 or num2 or num3 or num4 == 9:
-    numero9+=1
-    print(f'O valor 9 apareceu {numero9} vezes.')
-
-for pos,numeros in enumerate(numeros):
-    if numeros == 3:
-        print(f'O valor 3 apareceu na {pos+1}ª posição.')
-
-if num1  % 2 == 0:
-    pares += 1
-elif num2 % 2 == 0:
-    pares += 1
-elif num3 % 2 == 0:
-    pares += 1
-elif num4 % 2 == 0:
-    pares += 1
-
-
-
-print(f'Os valores pares digitados foram {pares}')
-
-
-
+num = (int(input('Digite um número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite mais um número: ')),
+       int(input('Digite o último número: ')),)
+print(f'Você digitou os valores {num}.')
+print(f'O valor 9 apareceu {num.count(9)} vezes.')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1}ª posição.')
+else:
+    print(f'O valor 3 não foi digitado em nenhuma posição.')
+print(f'Os valores pares digitados foram ',end=' ')
+for n in num:
+    if n % 2 ==0:
+        print(n,end=' ')
