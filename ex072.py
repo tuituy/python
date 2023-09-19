68d4cfdb6fd9438aeeee138dd7782efc166f0979
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-18 21:50:04 -0300

    Add files via upload

--- ex072.py
+++ ex072.py
@@ -1,6 +1,14 @@
-n = ('Zero','Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quatorze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
-num = ' '
-while num not in (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20):
+n = ('Zero','Um','Dois','Três','Quatro','Cinco','Seis','Sete',
+     'Oito','Nove','Dez','Onze','Doze','Treze','Quatorze','Quinze',
+     'Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
+continua = 'S'
+while continua == 'S':
     num = int(input('Digite um número entre 0 e 20: '))
-print(f'Você digitou o número {n[num]}')
+    print(f'Você digitou o número {n[num]}')
+    continua = (str(input('Você quer continuar? [S/N]'))).upper().strip()[0]
+
+    if continua != 'S':
+        break
+
+
 
