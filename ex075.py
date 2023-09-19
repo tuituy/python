17ab39124cff106834b8b0e03835938deaccf0c6
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-18 22:10:20 -0300

    Add files via upload

--- ex075.py
+++ ex075.py
@@ -1,7 +1,7 @@
 num = (int(input('Digite um número: ')),
        int(input('Digite outro número: ')),
        int(input('Digite mais um número: ')),
-       int(input('Digite o último número: ')),)
+       int(input('Digite o último número: ')))
 print(f'Você digitou os valores {num}.')
 print(f'O valor 9 apareceu {num.count(9)} vezes.')
 if 3 in num:
@@ -10,5 +10,5 @@ else:
     print(f'O valor 3 não foi digitado em nenhuma posição.')
 print(f'Os valores pares digitados foram ',end=' ')
 for n in num:
-    if n % 2 ==0:
+    if n % 2 == 0:
         print(n,end=' ')
