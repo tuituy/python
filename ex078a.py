70f2e327418be9a92bc8da0a185488fb84953466
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-21 22:51:25 -0300

    Add files via upload

--- ex078a.py
+++ ex078a.py
@@ -4,23 +4,23 @@ menor = []
 
 for c in range(0,5):
     valor.append(int(input(f'Digite um valor para a posição {c}: ')))
-    if c(valor.append()) > 0:
-      
+
+
 
 print(f'Você digitou os valores {valor}')
 
-if len(valor) == 1 :
+if len(valor) == 5 :
     maior = valor.append(0)
-    menor = valor.append(0)
+    menor = valor[0]
 
-#for c,v in enumerate(valor):
-    #if v > maior:
-        #print('oi')
-
-
+#for c,i in enumerate(valor):
+    #if i > maior:
+       # print(maior)
 
 
 
 
 
-print(maior,menor)
+
+
+print(maior)
