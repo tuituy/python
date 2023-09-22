446463cc4d8884c74d36241b86fdc7b4a72be31f
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-21 22:37:12 -0300

    Add files via upload

---
+++ ex078a.py
@@ -0,0 +1,26 @@
+valor = []
+maior = []
+menor = []
+
+for c in range(0,5):
+    valor.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c(valor.append()) > 0:
+      
+
+print(f'Você digitou os valores {valor}')
+
+if len(valor) == 1 :
+    maior = valor.append(0)
+    menor = valor.append(0)
+
+#for c,v in enumerate(valor):
+    #if v > maior:
+        #print('oi')
+
+
+
+
+
+
+
+print(maior,menor)
