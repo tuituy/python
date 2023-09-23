ad00b17fc725253c61a35ef64484b17a14a5ce4e
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-22 21:42:31 -0300

    Add files via upload

--- ex078a.py
+++ ex078a.py
@@ -1,6 +1,8 @@
 valor = []
-maior = []
-menor = []
+maior = 0
+menor = 0
+posicaomaior = []
+posicaomenor = []
 
 for c in range(0,5):
     valor.append(int(input(f'Digite um valor para a posição {c}: ')))
@@ -10,12 +12,15 @@ for c in range(0,5):
 print(f'Você digitou os valores {valor}')
 
 if len(valor) == 5 :
-    maior = valor.append(0)
+    maior = valor[0]
     menor = valor[0]
 
-#for c,i in enumerate(valor):
-    #if i > maior:
-       # print(maior)
+for c,i in enumerate(valor):
+    if valor[c] > maior:
+        maior = valor[c]
+        posicaomaior.append(c)
+        print(posicaomaior)
+        print(f'O maior valor é {maior} e está nas posições {posicaomaior}')
 
 
 
