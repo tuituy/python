708c1e82e9bf9f668caabae9f4c133da6ecec1db
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-23 22:01:01 -0300

    Add files via upload

---
+++ ex081.py
@@ -0,0 +1,27 @@
+lista = []
+num = 0
+cont = 'S'
+
+while cont in 'Ss':
+    num = int(input('Digite um valor: '))
+    lista.append(num)
+    cont = str(input('Quer continuar? [S/N]'))
+print(lista)
+
+print(f'Você digitou {len(lista)} elementos.')
+
+print(f'Os valores em ordem descrescente são {sorted(lista, reverse = True)}')
+
+pos = 0
+while pos != len(lista):
+    if 5 == lista[pos]:
+        print('O valor 5 faz parte da lista!')
+        break
+    elif pos >= len(lista):
+        print('O valor 5 não foi encontrado na lista!')
+    pos +=1
+
+
+
+
+
