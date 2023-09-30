9e27aaef8548ca9e31b72ba94fc74aee8f39835a
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-09-29 22:25:35 -0300

    Add files via upload

--- ex089.py
+++ ex089.py
@@ -5,6 +5,7 @@ media = []
 resp = 'S'
 
 
+
 while resp in 'S':
 
     nome = str(input('Nome: '))
@@ -28,7 +29,22 @@ print('Nº  NOME            MÉDIA')
 print('-'*30)
 
 for c in range (0,len(lista)):
-    print(f'{c}    {lista[c][0]}',f'{media[c]:<20}')
+    print(f'{c:<4}{lista[c][0]:<10}{media[c]:>8}')
+
+while resp2 != 999:
+    resp2.append(print(input('Mostrar notas de qual aluno? (999 interrompe):')))
+
+    while True:
+        print('-'*35)
+        opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+        if opc == 999:
+            print('FINALIZANDO...')
+            break
+        if opc <= len(lista):
+            print(f'Notas de {lista[opc][0]} são {lista[opc][1],lista[opc][2]}')
+
+
+
 
 
 
