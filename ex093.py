fba7a52ba29cd65db41866874853d2bfd78ef574
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-10-03 22:23:29 -0300

    Add files via upload

--- ex093.py
+++ ex093.py
@@ -8,7 +8,7 @@ for c in range(1,partidas+1):
     gols1 = int(input(f'Quantos golda na partida {c}? '))
     gols.append(gols1)
     total += gols1
-jogador['gols'] = gols
+jogador['gols'] = gols[:]
 jogador['total'] = total
 print('-='*30)
 print(jogador)
@@ -16,7 +16,7 @@ print('-='*30)
 for k,v in jogador.items():
     print(f'O campo {k} tem o valor {v}')
 print('-='*30)
-print(f'O jogador {jogador["nome"]} jogou {partidas}.')
+print(f'O jogador {jogador["nome"]} jogou {partidas} partidas.')
 for c in range(0,partidas):
-    print(f'     => Na partida {c}, fez {jogador["gols"][c]} gols. ')
+    print(f'     => Na partida {c+1}, fez {jogador["gols"][c]} gols. ')
 print(f'Foi um total de {jogador["total"]} gols.')
