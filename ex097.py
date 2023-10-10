5a53587ad5249ed6466ace83e6bf10a45b7f6cfd
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-10-09 21:44:01 -0300

    Add files via upload

---
+++ ex097.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tamanho = len(msg) +4
+    print('~'* tamanho)
+    print(f'  {msg}')
+    print('~' * tamanho)
+
+
+
+escreva('Arthur Borba')
+escreva('Curso de Python')
+escreva('Oi')
