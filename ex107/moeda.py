bc41194d874eca45175990f778f6fb30b1c47ba5
Arthur Borba <34126860+tuituy@users.noreply.github.com>
2023-10-18 22:14:28 -0300

    Add files via upload

---
+++ ex107/moeda.py
@@ -0,0 +1,20 @@
+def metade(n):
+    resp = n / 2
+    return resp
+
+
+def dobro(n):
+    resp = n * 2
+    return resp
+
+
+def aumentar(x,y):
+    porc = (x / 100) * y
+    resp = x + porc
+    return resp
+
+
+def diminuir(x,y):
+    porc = (x / 100) * y
+    resp = x - porc
+    return resp
